5f019d126c67864a0d0f3e6d6781e39399dee9a7
Kuei-chun Chen <simagix@gmail.com>
2019-05-12 13:27:29 -0400

    add TestDistinct

---
+++ examples/distinct_test.go
@@ -0,0 +1,44 @@
+// Copyright 2019 Kuei-chun Chen. All rights reserved.
+
+package examples
+
+import (
+	"context"
+	"testing"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func TestDistinct(t *testing.T) {
+	var err error
+	var client *mongo.Client
+	var collection *mongo.Collection
+	var cur *mongo.Cursor
+	var ctx = context.Background()
+	client = getMongoClient()
+	seedCarsData(client, dbName)
+	collection = client.Database(dbName).Collection(collectionName)
+	filter := bson.D{{}}
+
+	if cur, err = collection.Find(ctx, filter); err != nil {
+		t.Fatal(err)
+	}
+	defer cur.Close(ctx)
+	amap := map[string]string{}
+	var doc bson.M
+
+	for cur.Next(ctx) {
+		cur.Decode(&doc)
+		brand := doc["brand"].(string)
+		amap[brand] = brand
+	}
+
+	var brands []interface{}
+	if brands, err = collection.Distinct(ctx, "brand", filter); err != nil {
+		t.Fatal(err)
+	}
+	if len(brands) != len(amap) {
+		t.Fatal("find failed, expected", len(amap), "but got", len(brands))
+	}
+}
